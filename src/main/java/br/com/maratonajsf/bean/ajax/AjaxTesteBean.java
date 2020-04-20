9ced0cd8aaebe303fa79d4d7a951165683d4b5ea
Jaime Eduardo <jaimeduardo050396@hotmail.com>
2020-04-20 15:06:30 -0300

    Inicio requisições ajax

---
+++ src/main/java/br/com/maratonajsf/bean/ajax/AjaxTesteBean.java
@@ -0,0 +1,35 @@
+package br.com.maratonajsf.bean.ajax;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@ViewScoped
+public class AjaxTesteBean implements Serializable {
+    private String nome;
+    private String sobrenome;
+
+    public void toUpperCase(){
+        this.nome = this.nome.toUpperCase();
+        this.sobrenome = this.sobrenome.toUpperCase();
+        System.out.println(nome);
+        System.out.println(sobrenome);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+}
